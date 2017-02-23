0a293bf53942b438e74a43ed423c9e3e3f19033f
Helder Burato Berto <helder.burato@gmail.com>
2017-02-22 23:00:31 -0300

    UPDATE: Throw errors

--- src/services/local-storage-manager.js
+++ src/services/local-storage-manager.js
@@ -21,31 +21,33 @@ function localStorageManager($localStorage, prefix) {
   var output = {};
 
   output.getPrefixStorage = function() {
-
     return prefix.storage;
   };
 
   output.getStorage = function(key) {
-
-    if ($localStorage[prefix.storage+key]) {
-      return JSON.parse($localStorage[prefix.storage+key]);
+    if (!key) { 
+      throw new Error('Informe a key para retornar o indíce da localStorage'); 
     }
+
+    return JSON.parse($localStorage[prefix.storage+key]);
   };
 
   output.setStorage = function(obj, key) {
-
-    if (!$localStorage[prefix.storage+key]) {
-      $localStorage[prefix.storage+key] = JSON.stringify(obj);
+    if (!key) { 
+      throw new Error('Informe a key para acessar a localStorage'); 
     }
+
+    $localStorage[prefix.storage+key] = JSON.stringify(obj);
     return true;
   };
 
   output.clearStorage = function(key) {
-
-    if (key) {
-      delete $localStorage[prefix.storage+key];
-      return true;
+    if (!key) { 
+      throw new Error('Informe a key que deve ser deletada o índice da localStorage'); 
     }
+
+    delete $localStorage[prefix.storage+key];
+    return true;
   };
 
   return output;
